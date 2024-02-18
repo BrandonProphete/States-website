f4df6b8212148f3578a363a59f15d566d935eedd
Brandon Prophete <98864860+BrandonProphete@users.noreply.github.com>
2024-02-18 11:02:14 -0500

    Add files via upload

---
+++ flaskProject/app.py
@@ -0,0 +1,73 @@
+# Queens College
+# Internet and Web Technology  (CSCI 355)
+# Winter 2024
+# Assignment 11 - Web-Based Application
+# Brandon Prophete
+# Worked with Class
+
+import os
+import webbrowser
+
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+logged_in = False
+
+
+@app.route('/')
+def home_page():
+    return render_template('login.html')
+
+def read_file(file_name):
+    with open(file_name) as file:
+        lines = file.readlines()
+        states = [line.strip().split(",") for line in lines]
+        return states[0], states[1:]
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'Hello {name}!'
+
+@app.route('/states')
+def states():
+    title = 'The United States'
+    headers, data, = read_file('states.csv')
+    return render_template('results.html', title=title, headers=headers, data=data)
+
+@app.route('/my_state', methods=['GET', 'POST'])
+def my_state():
+    if request.method.upper() == 'POST':
+        state = request.form['state']
+        title = 'The United States'
+        headers, data = read_file('states.csv')
+        headers.append('selected')
+        for row in data:
+            row.append('')
+            if row[0] == state:
+                row[4] = '##############'
+        return render_template('results.html', title=title, headers=headers, data=data)
+
+@app.route('/login', methods=['POST'])
+def login():
+    global logged_in
+    if request.method.upper() == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+    if login == 'brandonprophete' and password == '355':
+        logged_in = True
+        headers, data = read_file('states.csv')
+        states = [row[0] for row in data]
+        return render_template('choose_state.html', states=states)
+        # return f'User {login} has successfully logged in.'
+    else:
+        return f'User {login} has been rejected.'
+
+def open_file_in_browser(file_name):
+    url = 'file:///' + os.getcwd() + '/' + file_name
+    print(url)
+    webbrowser.open_new_tab(url)
+
+
+if __name__ == '__main__':
+    app.run()
+
